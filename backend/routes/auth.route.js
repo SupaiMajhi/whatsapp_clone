2a214bdda4850f19184c9dc69bc91200fb81778e
Supai Majhi <sameertudu978@gmail.com>
2026-05-20 16:12:19 +0530

    add /update route in auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 
-import { getOtpHandler, resendHandler, verifyOtpHandler, checkAuthHandler, avatarUploadHandler, logoutHandler, checkVTtokenHandler } from "../controllers/auth.controller.js";
+import { getOtpHandler, resendHandler, verifyOtpHandler, checkAuthHandler, avatarUploadHandler, logoutHandler, checkVTtokenHandler, updateUserHandler } from "../controllers/auth.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import authLimiter from "../middleware/rateLimit.js";
 import upload from "../services/multer.js";
@@ -20,6 +20,9 @@ router.get('/check-auth', authMiddleware, checkAuthHandler);
 
 router.get('/check-vt', checkVTtokenHandler);
 
-router.post('/upload-profile', authMiddleware, upload.single('avatar'), avatarUploadHandler);
+//move this route to user.route.js
+router.patch('/upload-profile', authMiddleware, upload.single('avatar'), avatarUploadHandler);
+
+router.patch('/update', authMiddleware, upload.single('profilePic'), updateUserHandler)
 
 export default router;
