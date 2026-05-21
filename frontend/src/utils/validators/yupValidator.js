0f801018cbf4e30415176623ef83ff3a09965be6
Supai Majhi <sameertudu978@gmail.com>
2026-05-21 18:05:50 +0530

    move upward the fileType test in profileSetupSchema

--- frontend/src/utils/validators/yupValidator.js
+++ frontend/src/utils/validators/yupValidator.js
@@ -4,7 +4,7 @@ import * as yup from "yup";
 import countries from "../../../country.js";
 import { isValidPhoneNumber } from "libphonenumber-js/mobile";
 
-const types = ['image/png', 'image/jpg', 'image/jpeg'];
+export const types = ['image/png', 'image/jpg', 'image/jpeg'];
 
 export const loginSchema = yup.object({
     alpha2: yup.string().required("select country"),
@@ -17,13 +17,13 @@ export const loginSchema = yup.object({
 
 export const profileSetupSchema = yup.object({
     username: yup.string().required("Username is required.").min(6, "Minimum character length should be 6").max(10, "too long").matches(/^[a-zA-Z0-9]+$/, "Invalid username."),
-    profilePic: yup.mixed().test("fileSize", "Maximum size can be of 5MB", (value) => {
-        if(!value?.length) return true;
-
-        return value[0].size <= 5000000;
-    }).test("fileType", "File type is not supported.", (value) => {
+    profilePic: yup.mixed().test("fileType", "File type is not supported.", (value) => {
         if(!value?.length) return true;
 
         return types.includes(value[0].type);
+    }).test("fileSize", "Maximum size can be of 5MB", (value) => {
+        if(!value?.length) return true;
+
+        return value[0].size <= 5000000;
     })
 });
