e972d51fd947fe2bf8430af831e306efcc347778
Supai Majhi <sameertudu978@gmail.com>
2026-07-01 20:07:34 +0530

    fix: where chat list's formatted dates were not showing correctly

--- frontend/src/utils/util.js
+++ frontend/src/utils/util.js
@@ -1,84 +1,86 @@
-import useSocketStore from "../store/socketStore.js"
+import useSocketStore from "../store/socketStore.js";
 
 const dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 
+const timeOptions = {
+  calendar: "gregory",
+  timeZone: "+05:30",
+  hour12: true,
+  hour: "2-digit",
+  minute: "2-digit",
+};
+
+const dateOptions = {
+  calendar: "gregory",
+  timeZone: "+05:30",
+  year: "numeric",
+  month: "2-digit",
+  day: "numeric",
+};
+
 export const onSeen = (messagesIds) => {
-    const socket = useSocketStore.getState()?.socket;
-    if(socket?.readyState === WebSocket.OPEN){
-        socket?.send(JSON.stringify({
-            type: 'markAsSeen',
-            content: {
-                data: messagesIds
-            }
-        }))
-    }
-}
+  const socket = useSocketStore.getState()?.socket;
+  if (socket?.readyState === WebSocket.OPEN) {
+    socket?.send(
+      JSON.stringify({
+        type: "markAsSeen",
+        content: {
+          data: messagesIds,
+        },
+      }),
+    );
+  }
+};
 
 export const sendMessageViaSocket = (msgType, content) => {
-    const socket = useSocketStore.getState()?.socket;
-    if(socket?.readyState === WebSocket.OPEN){    
-        console.log('sent');
-        socket?.send(JSON.stringify({
-            type: msgType,
-            content
-        }));
-    }
-}
+  const socket = useSocketStore.getState()?.socket;
+  if (socket?.readyState === WebSocket.OPEN) {
+    console.log("sent");
+    socket?.send(
+      JSON.stringify({
+        type: msgType,
+        content,
+      }),
+    );
+  }
+};
 
 export const formatChatTime = (time) => {
+  if (time) {
     const now = new Date();
     const chatTime = new Date(time);
 
-    const diff = now.getDate() - chatTime.getDate();
-
-    if(diff === 0){
-        return chatTime.toLocaleTimeString("en-IN", {
-            hour: "2-digit",
-            minute: "2-digit"
-        });
-    } else if(diff === 1){
-
-        return "Yesterday";
-    } else if(diff > 1 && diff < 7){
-
-        const dayVal = chatTime.getDay();
-        return dayNames[dayVal];
-    } else if(diff > 6){
-
-        return chatTime.toLocaleDateString("en-IN", {
-            hour: "2-digit",
-            minute: "2-digit"
-        });
+    // calculating hours from milliseconds
+    const hours = Math.floor((now.getTime() - chatTime.getTime()) / 3600000);
+    if (hours <= 24) {
+      return new Intl.DateTimeFormat("en-IN", timeOptions).format(chatTime);
+    } else if (hours > 24 && hours <= 48) {
+      return "Yesterday";
+    } else if (hours > 48 && hours <= 168) {
+      const dayVal = chatTime.getDay();
+      return dayNames[dayVal];
     } else {
-        return "NaN"
+      return new Intl.DateTimeFormat("en-IN", dateOptions).format(chatTime);
     }
-}
+  }
+};
 
 export const formatMessageTime = (time) => {
+  if (time) {
     const msgTime = new Date(time);
-    const now = new Date();
+    const dtf = new Intl.DateTimeFormat(
+      ["en-IN", "en-GB", "en-US"],
+      timeOptions,
+    );
 
-    if(msgTime.toDateString() === now.toDateString()){
-        return msgTime.toLocaleTimeString("en-IN", {
-            hour: "2-digit",
-            minute: "2-digit"
-        });
-    }
-
-    const yesterday = new Date();
-    yesterday.setDate(now.getDate() - 1);
-
-    if(yesterday.toDateString() === msgTime.toDateString()){
-        return 'Yesterday';
-    }
-
-    return 'NaN';
-}
+    return dtf.format(msgTime);
+  }
+};
